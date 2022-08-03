2256e48ac49f15fe95e776ca5aaac161f38d2cc4
kyowoii23 <kyowoii23@gmail.com>
2022-08-03 17:55:12 +0900

    API_HOST 수정

--- common/index.js
+++ common/index.js
@@ -1,2 +1,2 @@
-export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'https://onceindustry-blog-front-qdypojkh5q-an.a.run.app/';
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'https://onceindustry-qdypojkh5q-an.a.run.app/';
 
